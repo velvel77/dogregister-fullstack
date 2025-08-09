ca68d88fcfe4b0a7cd5e43b2c7dddccfe8232448
Velvet <velvet.pinkish@gmail.com>
2025-08-09 14:33:22 +0200

    Add CORS feature for Angular support

---
+++ api/src/main/java/io/github/velvel77/dogregister/api/config/WebConfigProd.java
@@ -0,0 +1,37 @@
+package io.github.velvel77.dogregister.api.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.time.Duration;
+import java.util.List;
+
+@Configuration
+@Profile("prod")
+public class WebConfigProd {
+
+    @Value("${app.cors.allowed-origin}")
+    private String allowedOrigin;
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of(allowedOrigin));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        //Reduce option calls with preflight responses
+        config.setMaxAge(Duration.ofHours(1));
+
+        return source;
+    }
+}
