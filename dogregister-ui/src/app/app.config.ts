3ff9227d63178840fbe3ffa280743c49827e672c
Velvet <velvet.pinkish@gmail.com>
2025-10-12 18:34:13 +0200

    Add lucide icons for buttons

--- dogregister-ui/src/app/app.config.ts
+++ dogregister-ui/src/app/app.config.ts
@@ -1,14 +1,18 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient} from '@angular/common/http';
+import { LucideAngularModule, X, Trash2, Plus } from 'lucide-angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
     provideClientHydration(),
-    provideHttpClient()
+    provideHttpClient(),
+    importProvidersFrom(
+      LucideAngularModule.pick({ X, Trash2, Plus })
+    )
   ]
 };
